acd4e30bc2c4de32215a01b2a467cb2be6af4730
HailMuffins <rafaelsm123@outlook.com>
2022-08-24 10:20:55 -0300

    Requisito Dois Passou Nos Testes

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,17 +1,43 @@
 import React, { Component } from 'react';
 
 class Form extends Component {
+  constructor(props) {
+    super(props);
+  }
   render() {
+    const {
+      cardName,
+      cardDescription,
+      cardAttr1,
+      cardAttr2,
+      cardAttr3,
+      cardImage,
+      cardRare,
+      cardTrunfo,
+      hasTrunfo,
+      isSaveButtonDisabled,
+      onInputChange,
+      onSaveButtonClick,
+    } = this.props;
     return (
       <div>
         <form>
           <label htmlFor='carta'>
-            Carta: <input type='text' id='carta' data-testid='name-input' />
+            Carta:{' '}
+            <input
+              onChange={onInputChange}
+              value={cardName}
+              type='text'
+              id='carta'
+              data-testid='name-input'
+            />
           </label>
 
           <label htmlFor='descrição'>
             Descrição:{' '}
             <textarea
+              onChange={onInputChange}
+              value={cardDescription}
               name='descrição'
               id='descrição'
               cols='30'
@@ -22,26 +48,56 @@ class Form extends Component {
 
           <label htmlFor='pri-atri'>
             Primeiro Atributo:{' '}
-            <input type='number' data-testid='attr1-input' id='pri-atri' />
+            <input
+              onChange={onInputChange}
+              value={cardAttr1}
+              type='number'
+              data-testid='attr1-input'
+              id='pri-atri'
+            />
           </label>
 
           <label htmlFor='seg-atri'>
             Segundo Atributo:{' '}
-            <input type='number' data-testid='attr2-input' id='seg-atri' />
+            <input
+              onChange={onInputChange}
+              value={cardAttr2}
+              type='number'
+              data-testid='attr2-input'
+              id='seg-atri'
+            />
           </label>
 
           <label htmlFor='ter-atri'>
             Terceiro Atributo:{' '}
-            <input type='number' data-testid='attr3-input' id='ter-atri' />
+            <input
+              onChange={onInputChange}
+              value={cardAttr3}
+              type='number'
+              data-testid='attr3-input'
+              id='ter-atri'
+            />
           </label>
 
           <label htmlFor='imagem'>
-            Imagem: <input type='file' data-testid='image-input' id='imagem' />
+            Imagem:{' '}
+            <input
+              onChange={onInputChange}
+              value={cardImage}
+              type='text'
+              data-testid='image-input'
+              id='imagem'
+            />
           </label>
 
           <label htmlFor='raridade'>
             Raridade:{' '}
-            <select data-testid='rare-input' id='raridade'>
+            <select
+              onChange={onInputChange}
+              value={cardRare}
+              data-testid='rare-input'
+              id='raridade'
+            >
               <option value='normal'>Normal</option>
               <option value='raro'>Raro</option>
               <option value='muito raro'>Muito Raro</option>
@@ -51,12 +107,19 @@ class Form extends Component {
           <label htmlFor='super-trunfo'>
             Super Trunfo:
             <input
+              onChange={onInputChange}
+              checked={cardTrunfo}
               type='checkbox'
               data-testid='trunfo-input'
               id='super-trunfo'
             />
           </label>
-          <button type='submit' data-testid='save-button'>
+          <button
+            onClick={onSaveButtonClick}
+            disabled={isSaveButtonDisabled}
+            type='submit'
+            data-testid='save-button'
+          >
             Salvar
           </button>
         </form>
