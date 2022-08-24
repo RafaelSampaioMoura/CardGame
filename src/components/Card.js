3b37d1c29ad0828596fa0ac9acfa9f27077ae16a
HailMuffins <rafaelsm123@outlook.com>
2022-08-24 16:43:05 -0300

    Refatoração Lint

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 // import Form from './Form';
 
 export class Card extends Component {
@@ -36,17 +37,29 @@ export class Card extends Component {
 
     return (
       <div>
-        <h1 data-testid='name-card'>{cardName}</h1>
-        <img data-testid='image-card' src={cardImage} alt={cardName} />
-        <p data-testid='description-card'>{cardDescription}</p>
-        <p data-testid='attr1-card'>{cardAttr1}</p>
-        <p data-testid='attr2-card'>{cardAttr2}</p>
-        <p data-testid='attr3-card'>{cardAttr3}</p>
-        <p data-testid='rare-card'>{cardRare}</p>
-        {cardTrunfo && <p data-testid='trunfo-card'>Super Trunfo</p>}{' '}
+        <h1 data-testid="name-card">{cardName}</h1>
+        <img data-testid="image-card" src={ cardImage } alt={ cardName } />
+        <p data-testid="description-card">{cardDescription}</p>
+        <p data-testid="attr1-card">{cardAttr1}</p>
+        <p data-testid="attr2-card">{cardAttr2}</p>
+        <p data-testid="attr3-card">{cardAttr3}</p>
+        <p data-testid="rare-card">{cardRare}</p>
+        {cardTrunfo && <p data-testid="trunfo-card">Super Trunfo</p>}
+        {' '}
       </div>
     );
   }
 }
+
+Card.propTypes = {
+  cardName: PropTypes.string,
+  cardDescription: PropTypes.string,
+  cardAttr1: PropTypes.string,
+  cardAttr2: PropTypes.string,
+  cardAttr3: PropTypes.string,
+  cardImage: PropTypes.string,
+  cardRare: PropTypes.string,
+  cardTrunfo: PropTypes.bool,
+}.isRequired;
 
 export default Card;
--- src/components/Form.js
+++ src/components/Form.js
@@ -56,133 +56,151 @@ class Form extends Component {
       onSaveButtonClick,
     } = this.props;
 
+    const {
+      nome: name,
+      descrição: description,
+      atributoUm: attOne,
+      atributoDois: attTwo,
+      atributoTres: attThree,
+      imagem: imageAddress,
+      raridade: rarity,
+      trunfo: trump,
+    } = this.state;
+
     return (
       <div>
         <pre>{JSON.stringify(this.state, undefined, 2)}</pre>
-        <form onSubmit={this.handleSubmit}>
-          <label htmlFor='carta'>
-            Carta:{' '}
+        <form onSubmit={ this.handleSubmit }>
+          <label htmlFor="carta">
+            Carta:
+            {' '}
             <input
-              name='nome'
-              onChange={onInputChange}
-              value={cardName}
-              type='text'
-              id='carta'
-              data-testid='name-input'
+              name="nome"
+              onChange={ onInputChange }
+              value={ cardName }
+              type="text"
+              id="carta"
+              data-testid="name-input"
             />
           </label>
 
-          <label htmlFor='descrição'>
-            Descrição:{' '}
+          <label htmlFor="descrição">
+            Descrição:
+            {' '}
             <textarea
-              onChange={onInputChange}
-              value={cardDescription}
-              name='descrição'
-              id='descrição'
-              cols='30'
-              rows='10'
-              data-testid='description-input'
+              onChange={ onInputChange }
+              value={ cardDescription }
+              name="descrição"
+              id="descrição"
+              cols="30"
+              rows="10"
+              data-testid="description-input"
             />
           </label>
 
-          <label htmlFor='atributoUm'>
-            Primeiro Atributo:{' '}
+          <label htmlFor="atributoUm">
+            Primeiro Atributo:
+            {' '}
             <input
-              name='atributoUm'
-              onChange={onInputChange}
-              value={cardAttr1}
-              type='number'
-              data-testid='attr1-input'
-              id='atributoUm'
-              min='0'
-              max='90'
+              name="atributoUm"
+              onChange={ onInputChange }
+              value={ cardAttr1 }
+              type="number"
+              data-testid="attr1-input"
+              id="atributoUm"
+              min="0"
+              max="90"
             />
           </label>
 
-          <label htmlFor='seg-atri'>
-            Segundo Atributo:{' '}
+          <label htmlFor="seg-atri">
+            Segundo Atributo:
+            {' '}
             <input
-              name='atributoDois'
-              onChange={onInputChange}
-              value={cardAttr2}
-              type='number'
-              data-testid='attr2-input'
-              id='seg-atri'
-              min='0'
-              max='90'
+              name="atributoDois"
+              onChange={ onInputChange }
+              value={ cardAttr2 }
+              type="number"
+              data-testid="attr2-input"
+              id="seg-atri"
+              min="0"
+              max="90"
             />
           </label>
 
-          <label htmlFor='ter-atri'>
-            Terceiro Atributo:{' '}
+          <label htmlFor="ter-atri">
+            Terceiro Atributo:
+            {' '}
             <input
-              name='atributoTres'
-              onChange={onInputChange}
-              value={cardAttr3}
-              type='number'
-              data-testid='attr3-input'
-              id='ter-atri'
-              min='0'
-              max='90'
+              name="atributoTres"
+              onChange={ onInputChange }
+              value={ cardAttr3 }
+              type="number"
+              data-testid="attr3-input"
+              id="ter-atri"
+              min="0"
+              max="90"
             />
           </label>
 
-          <label htmlFor='imagem'>
-            Imagem:{' '}
+          <label htmlFor="imagem">
+            Imagem:
+            {' '}
             <input
-              name='imagem'
-              onChange={onInputChange}
-              value={cardImage}
-              type='text'
-              data-testid='image-input'
-              id='imagem'
+              name="imagem"
+              onChange={ onInputChange }
+              value={ cardImage }
+              type="text"
+              data-testid="image-input"
+              id="imagem"
             />
           </label>
 
-          <label htmlFor='raridade'>
-            Raridade:{' '}
+          <label htmlFor="raridade">
+            Raridade:
+            {' '}
             <select
-              name='raridade'
-              onChange={onInputChange}
-              value={cardRare}
-              data-testid='rare-input'
-              id='raridade'
+              name="raridade"
+              onChange={ onInputChange }
+              value={ cardRare }
+              data-testid="rare-input"
+              id="raridade"
             >
-              <option value='normal'>Normal</option>
-              <option value='raro'>Raro</option>
-              <option value='muito raro'>Muito Raro</option>
+              <option value="normal">Normal</option>
+              <option value="raro">Raro</option>
+              <option value="muito raro">Muito Raro</option>
             </select>
           </label>
 
-          <label htmlFor='super-trunfo'>
+          <label htmlFor="super-trunfo">
             Super Trunfo:
             <input
-              name='trunfo'
-              onChange={onInputChange}
-              checked={cardTrunfo}
-              type='checkbox'
-              data-testid='trunfo-input'
-              id='super-trunfo'
+              name="trunfo"
+              onChange={ onInputChange }
+              checked={ cardTrunfo }
+              type="checkbox"
+              data-testid="trunfo-input"
+              id="super-trunfo"
             />
           </label>
           <button
-            onClick={onSaveButtonClick}
-            disabled={isSaveButtonDisabled}
-            type='submit'
-            data-testid='save-button'
+            onClick={ onSaveButtonClick }
+            disabled={ isSaveButtonDisabled }
+            type="submit"
+            data-testid="save-button"
           >
             Salvar
           </button>
         </form>
         <Card
-          cardName={this.state.nome}
-          cardDescription={this.state.descrição}
-          cardAttr1={this.state.atributoUm}
-          cardAttr2={this.state.atributoDois}
-          cardAttr3={this.state.atributoTres}
-          cardImage={this.state.imagem}
-          cardRare={this.state.raridade}
-          cardTrunfo={this.state.trunfo}
+          cardName={ name }
+          cardDescription={ description }
+          cardAttr1={ attOne }
+          cardAttr2={ attTwo }
+          cardAttr3={ attThree }
+          cardImage={ imageAddress }
+          cardRare={ rarity }
+          cardTrunfo={ trump }
         />
       </div>
     );
